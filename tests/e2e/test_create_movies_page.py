118bca44638149ed50d5ccaaaf4914f3911eaf73
CarsonWebb <carsonwebb14@gmail.com>
2023-03-18 17:07:01 -0400

    Completed tests

--- tests/e2e/test_create_movies_page.py
+++ tests/e2e/test_create_movies_page.py
@@ -1,1 +1,11 @@
 # TODO: Feature 2
+from src.repositories.movie_repository import get_movie_repository
+from src.models.movie import Movie
+
+def test_create_movies(test_app):
+    _movie_repo= get_movie_repository()
+    dict = {'title': 'Jurrasic Park','director': 'Steven Spielberg','rating': '4'}
+    response = test_app.post('/movies', data=dict)
+    assert _movie_repo.get_movie_by_title('Jurrasic Park')!=None
+    assert response.request.path == "/movies"
+    assert response.status_code == 302
--- tests/unit/test_create_movie.py
+++ tests/unit/test_create_movie.py
@@ -1,1 +1,13 @@
 # TODO: Feature 2
+from src.repositories.movie_repository import get_movie_repository
+from src.models.movie import Movie
+
+
+def test_create_movie():
+    _movie_repo= get_movie_repository()
+    movie = _movie_repo.create_movie('Jurrasic Park', 'Steven Spielberg', 4)
+    assert movie.title == 'Jurrasic Park'
+    assert movie.director == 'Steven Spielberg'
+    assert movie.rating == 4
+    assert type(movie) == Movie
+    assert _movie_repo.get_movie_by_title('Jurrasic Park') != None
