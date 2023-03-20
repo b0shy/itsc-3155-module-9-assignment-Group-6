e48b803a06d451408800472ae616dd22aca09847
LennyVolpe <122924156+LennyVolpe@users.noreply.github.com>
2023-03-20 14:33:53 -0400

    Search movies and tests (#8)

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template,request
 
 from src.repositories.movie_repository import get_movie_repository
 
@@ -35,4 +35,6 @@ def create_movie():
 @app.get('/movies/search')
 def search_movies():
     # TODO: Feature 3
-    return render_template('search_movies.html', search_active=True)
+    title = request.args.get('title')
+    searched_movie = movie_repository.get_movie_by_title(title)
+    return render_template('search_movies.html', search_active=True, searched_movie=searched_movie)
